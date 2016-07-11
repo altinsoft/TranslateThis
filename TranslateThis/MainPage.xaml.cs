ff8a8ea41d7cca515f624ea074e1996d213326c2
Mayur Tendulkar <mayur.tendulkar@hotmail.com>
2016-07-11 21:22:34 +0530

    TranslateThis Project Created

---
+++ TranslateThis/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TranslateThis
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TranslateButton_OnClicked(object sender, EventArgs e)
+        {
+            TranslateService service = new TranslateService();
+            var translatedText = await service.TranslateString(FromEntry.Text, "de");
+            TranslatedText.Text = translatedText;
+        }
+    }
+}
---
+++ TranslateThis/Model/DataMarketAccessToken.cs
@@ -0,0 +1,15 @@
+﻿namespace TranslateThis.Model
+{
+    public class DataMarketAccessToken
+    {
+       
+            public string access_token { get; set; }
+
+            public string token_type { get; set; }
+
+            public string expires_in { get; set; }
+
+            public string scope { get; set; }
+        
+    }
+}
